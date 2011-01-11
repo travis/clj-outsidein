5257f781bf029f1a6679582066e241436b5f8cce
Travis Vachon <travis.vachon@gmail.com>
2011-01-10 22:57:30 -0500

    Declare 1.0.0
    Things seem reasonably stable: 1.0.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-outsidein "1.0.0-SNAPSHOT"
+(defproject clj-outsidein "1.0.0"
   :description "A Clojure library for the Outside.in API"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
