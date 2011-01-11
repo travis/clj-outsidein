e272acb70dc9c96b2d373e4f711e0a9d8384934d
Travis Vachon <travis.vachon@gmail.com>
2011-01-10 22:50:22 -0500

    README population, move examples there.

--- src/outsidein/core.clj
+++ src/outsidein/core.clj
@@ -60,38 +60,3 @@
 (defn locations-resource
   ([root name]
      (oi-subresource root "locations/named/%s" name)))
-
-(comment
-
-  ;; examples:
-
-  (def root (oi-resource (oi-http-client "key" "secret")))
-
-  (map
-   :summary
-   (:stories
-    (read-json
-     (:body (GET (stories-resource root "NY"))))))
-
-  (use '[resrc-client.core :only [?q]])
-  (count (:stories (read-json (:body (GET (stories-by-zipcode-resource root 94117) (?q :limit 20))))))
-
-  (:stories (read-json (:body (GET (stories-by-zipcode-resource root 94117) (?q :page 2)))))
-  (:stories (read-json (:body (GET (stories-by-zipcode-resource root 94117) (?q :radius 900)))))
-  (:stories (read-json (:body (GET (stories-by-zipcode-resource root 94117) (?q :places-only true)))))
-
-  ;; story finder
-  (GET (stories-by-uuid-resource
-        root
-        (:uuid (first (:locations (read-json (:body (GET (locations-resource
-                                                          root
-                                                          "ithaca,ny")))))))))
-
-  ;; news about the first 5 springfields returned
-  (GET (apply stories-by-uuid-resource
-              root
-              (take 5 (map :uuid (:locations (read-json (:body (GET (locations-resource
-                                                                     root
-                                                                     "springfield")))))))))
-
-)
